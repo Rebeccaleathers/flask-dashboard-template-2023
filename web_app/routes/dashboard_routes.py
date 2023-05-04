64b9b66b8b70217d4ed7e32365f42a446932e81a
Rebeccaleathers <RGL32@GEORGETOWN.EDU>
2023-05-04 16:40:12 -0400

    fetching income data

--- web_app/routes/dashboard_routes.py
+++ web_app/routes/dashboard_routes.py
@@ -1,8 +1,9 @@
 
-
+import requests
+import json
 from flask import Blueprint, request, render_template, redirect
 
-from web_app.services.alpha import AlphavantageService
+from web_app.services.alpha import AlphavantageService, ALPHAVANTAGE_API_KEY
 
 
 dashboard_routes = Blueprint("dashboard_routes", __name__)
@@ -56,3 +57,47 @@ def unemployment_dashboard():
         print("ERROR", err)
         #flash("OOPS", "warning")
         return redirect("/")
+    
+@dashboard_routes.route("/income/form")
+def income_form():
+    print("INCOME FORM...")
+    return render_template("stocks_form.html")
+
+
+
+
+
+
+@dashboard_routes.route("/income/dashboard", methods=["GET", "POST"])
+def income_dashboard():
+    print("INCOME DASHBOARD...")
+
+    # if the form sends the data via POST request, we'll have request.form
+    # otherwise if we specify url params in a GET request, we'll have request.args
+    request_data = dict(request.form or request.args)
+    print("REQUEST DATA:", request_data)
+
+
+
+
+    try:
+        
+        symbol = request_data.get("symbol") or "MSFT"
+        print("SYMBOL:", symbol)
+
+        # your app can request data like this:
+        request_url = f"https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}"
+        response = requests.get(request_url)
+        data = json.loads(response.text)
+
+
+        # pass this data to the page:
+        print(data)
+        return render_template("income_dashboard.html", symbol=symbol, data=data)
+        
+    except Exception as err:
+        print("ERROR", err)
+        #flash("OOPS", "warning")
+        return redirect("/stocks/form")
+    
+
